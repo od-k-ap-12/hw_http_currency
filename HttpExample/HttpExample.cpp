605b28ae242f11522cfdeef4fee92ae0946b081f
Oleksandra <predictormagnolia@gmail.com>
2022-12-28 20:54:20 +0200

    Add project files.

---
+++ HttpExample/HttpExample.cpp
@@ -0,0 +1,124 @@
+﻿#pragma comment (lib, "Ws2_32.lib")
+#include <Winsock2.h>
+#include <ws2tcpip.h>
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "ru");
+
+    //1. инициализация "Ws2_32.dll" для текущего процесса
+    WSADATA wsaData;
+    WORD wVersionRequested = MAKEWORD(2, 2);
+
+    int err = WSAStartup(wVersionRequested, &wsaData);
+    if (err != 0) {
+
+        cout << "WSAStartup failed with error: " << err << endl;
+        return 1;
+    }  
+
+    //инициализация структуры, для указания ip адреса и порта сервера с которым мы хотим соединиться
+   
+    char hostname[255] = "api.openweathermap.org";
+    
+    addrinfo* result = NULL;    
+    
+    addrinfo hints;
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    int iResult = getaddrinfo(hostname, "http", &hints, &result);
+    if (iResult != 0) {
+        cout << "getaddrinfo failed with error: " << iResult << endl;
+        WSACleanup();
+        return 3;
+    }     
+
+    SOCKET connectSocket = INVALID_SOCKET;
+    addrinfo* ptr = NULL;
+
+    //Пробуем присоединиться к полученному адресу
+    for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
+
+        //2. создание клиентского сокета
+        connectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+        if (connectSocket == INVALID_SOCKET) {
+            printf("socket failed with error: %ld\n", WSAGetLastError());
+            WSACleanup();
+            return 1;
+        }
+
+       //3. Соединяемся с сервером
+        iResult = connect(connectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+        if (iResult == SOCKET_ERROR) {
+            closesocket(connectSocket);
+            connectSocket = INVALID_SOCKET;
+            continue;
+        }
+        break;
+    }
+
+    //4. HTTP Request
+
+   string uri = "/data/2.5/weather?q=Odessa&appid=75f6e64d49db78658d09cb5ab201e483&mode=JSON";
+    
+
+    string request = "GET " + uri + " HTTP/1.1\n"; 
+    request += "Host: " + string(hostname) + "\n";
+    request += "Accept: */*\n";
+    request += "Accept-Encoding: gzip, deflate, br\n";   
+    request += "Connection: keep-alive\n";   
+    request += "\n";
+
+    //отправка сообщения
+    if (send(connectSocket, request.c_str(), request.length(), 0) == SOCKET_ERROR) {
+        cout << "send failed: " << WSAGetLastError() << endl;
+        closesocket(connectSocket);
+        WSACleanup();
+        return 5;
+    }
+    cout << "send data" << endl;
+
+    //5. HTTP Response
+
+    string response;
+
+    const size_t BUFFERSIZE = 1024;
+    char resBuf[BUFFERSIZE];
+
+    int respLength;
+
+    do {
+        respLength = recv(connectSocket, resBuf, BUFFERSIZE, 0);
+        if (respLength > 0) {
+            response += string(resBuf).substr(0, respLength);           
+        }
+        else {
+            cout << "recv failed: " << WSAGetLastError() << endl;
+            closesocket(connectSocket);
+            WSACleanup();
+            return 6;
+        }
+
+    } while (respLength == BUFFERSIZE);
+
+    cout << response << endl;
+
+    //отключает отправку и получение сообщений сокетом
+    iResult = shutdown(connectSocket, SD_BOTH);
+    if (iResult == SOCKET_ERROR) {
+        cout << "shutdown failed: " << WSAGetLastError() << endl;
+        closesocket(connectSocket);
+        WSACleanup();
+        return 7;
+    }
+
+    closesocket(connectSocket);
+    WSACleanup();
+}
---
+++ HttpGetHtmlPAge/HttpGetHtmlPAge.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+#include <fstream>
+#include <codecvt>
+#pragma comment(lib, "urlmon.lib")
+#include <windows.h>
+using namespace std;
+
+
+wstring JSONFind(wstring& Text, wstring& Find);
+
+int main()
+{
+    SetConsoleOutputCP(65001); //Set console encoding to utf8
+
+    setlocale(LC_ALL, "ru");
+    // the URL to download from
+    const char* srcURL1 = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
+
+    // the destination file
+    const char* destFile1 = "currencybankgovua.txt";
+
+    // URLDownloadToFile returns S_OK on success
+    if (S_OK == URLDownloadToFileA(NULL, srcURL1, destFile1, 0, NULL))
+    {
+        cout << "Saved to " << destFile1 << "\n";
+    }
+
+    const char* srcURL2 = "https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5";
+
+    // the destination file
+    const char* destFile2 = "currencyprivatbankua.txt";
+
+    // URLDownloadToFile returns S_OK on success
+    if (S_OK == URLDownloadToFileA(NULL, srcURL2, destFile2, 0, NULL))
+    {
+        cout << "Saved to " << destFile2 << "\n";
+    }
+
+
+
+
+    wstring txt = TEXT("\"txt\"");
+    wstring rate = TEXT("\"rate\"");
+    wstring ccy = TEXT("\"ccy\"");
+    wstring baseccy = TEXT("\"base_ccy\"");
+    wstring buy = TEXT("\"buy\"");
+    wstring sale = TEXT("\"sale\"");
+    wstring TextFile;
+    wstring Line;
+    wifstream File;
+    File.open("currencybankgovua.txt");
+    File.imbue(std::locale(std::locale::empty(), new std::codecvt_utf8<wchar_t>));
+    while (getline(File, Line)) {
+        TextFile += Line;
+        TextFile += TEXT(" ");
+        wcout<<JSONFind(Line,txt);
+        cout << " ";
+        wcout<<JSONFind(Line, rate);
+        cout << endl;
+    }
+    File.close();
+
+    File.open("currencyprivatbankua.txt");
+    File.imbue(std::locale(std::locale::empty(), new std::codecvt_utf8<wchar_t>));
+    while (getline(File, Line)) {
+        TextFile += Line;
+        TextFile += TEXT(" ");
+        cout << "ccy: ";
+        wcout << JSONFind(Line, ccy);
+        cout << " base_ccy: ";
+        wcout << JSONFind(Line, baseccy);
+        cout << " buy: ";
+        wcout << JSONFind(Line, buy);
+        cout << " sale:";
+        wcout << JSONFind(Line, sale);
+        cout << endl;
+    }
+    File.close();
+}
+
+wstring JSONFind(wstring& Text, wstring& Find) {
+    wstring Data;
+    if (Text.find(Find) == wstring::npos) return TEXT("");
+    for (auto i = Text.begin() + Text.find(Find) + Find.length() + 1; *i != ','&&*i!='}'; i++) {
+        Data += *i;
+    }
+    return Data;
+}
